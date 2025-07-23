9e0bbd323387a32f61d6a4e1af1255094b3a0230
Prakash Raut <prakash_raut@outlook.com>
2025-07-23 14:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ class Person {
     this.age = age;
   }
 
-  getName() {
+  get name() {
     return this.name;
   }
 
-  setAge(age) {
+  set name(name) {
+    this.name = name;
+  }
+
+  get age() {
+    return this.age;
+  }
+	
+  set age(age) {
     this.age = age;
   }
 }
